0b62b5e81ace51040ea1b5d80e0fd4d3cda6eabc
Lucas Ezio Fraga <lucasf@epidataconsulting.com>
2023-05-05 18:52:35 +0200

    first comit

---
+++ src/main/java/com/aizoon/fabrickproyect/FabrickProyectApplication.java
@@ -0,0 +1,13 @@
+package com.aizoon.fabrickproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabrickProyectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabrickProyectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aizoon/fabrickproyect/FabrickProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aizoon.fabrickproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickProyectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
